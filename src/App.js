9fc7cbce37b933aab21c35a3742c138d9bf61222
varshini2809 <94052008+varshini2809@users.noreply.github.com>
2024-07-05 09:54:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Navbar from "./components/cards/NavBar";
 import TODOList from "./Simple_Music_Player/TODOList";
 
 function App() {
   return (
     <>
-      {/* <h1>hello</h1> */}
-      <Navbar></Navbar>
       <TODOList></TODOList>
     </>
   );
